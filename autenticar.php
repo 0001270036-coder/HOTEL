2c1019fe664346f4b9e1154d2b1fad0dd1b571c2
Luiza <0001270036@senaimgaluno.com.br>
2026-09-09 22:06:24 -0300

    Criei arquivo autenticar.php e arquivos de cadastro_cliente.html e minhas reservas.php

--- autenticar.php
+++ autenticar.php
@@ -1,47 +1,20 @@
- <?php
-session_start();
+<?php
+//session_start();
 include("conexao.php");
 
-// 2. Receber email e senha via POST
-$email = $_POST['email'];
-$senha_digitada = $_POST['senha'];
 
-// 3. Buscar cliente no banco pelo e-mail
-// usando prepare pra evitar SQL Injection
-$sql = "SELECT * FROM clientes WHERE email = ?";
-$stmt = $conexao->prepare($sql);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$resultado = $stmt->get_result();
+$email = $_POST['email_usuario'];
+$senha_digitada = $_POST['senha_usuario'];
 
-// 4. Verificar se usuário existe e comparar senha
-if ($resultado->num_rows == 1) {
-    $cliente = $resultado->fetch_assoc();
-    
-    // Verifica a senha criptografada
-    // Se no seu banco a senha não está criptografada ainda, use: if ($senha_digitada == $cliente['senha'])
-    if (password_verify($senha_digitada, $cliente['senha'])) {
-        
-        // 5. Se os dados forem válidos - cria a sessão e redireciona
-        $_SESSION['id_cliente'] = $cliente['id'];
-        $_SESSION['nome_cliente'] = $cliente['nome'];
-        $_SESSION['email_cliente'] = $cliente['email'];
+$sql = "SELECT * FROM clientes WHERE email = '$email' and senha = '$senha_digitada'";
+$resultado = mysqli_query($conexao,$sql);
 
-        header("Location: minhas_reservas.php");
-        exit();
 
-    } else {
-        // Senha incorreta
-        echo "Senha incorreta! <br>";
-        echo "<a href='login.html'>Voltar para o login</a>";
-    }
-
+if (mysqli_num_rows($resultado) > 0) {
+    header("Location: minhas_reservas.php");
+    exit();
 } else {
-    // 6. E-mail não encontrado
-    echo "E-mail não encontrado! <br>";
-    echo "<a href='login.html'>Voltar para o login</a>";
+    header("Location: login.html");
+    exit();
 }
-
-$stmt->close();
-$conexao->close();
 ?>
